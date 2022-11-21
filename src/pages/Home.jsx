b58f937f6ab321ea67bb3a0c6efa1b1ed74adda8
cindyeme <emereninicynthia@gmail.com>
2022-11-21 15:06:32 +0100

    updated details page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -83,7 +83,7 @@ export default function Home() {
       </div>
 
       <div className="py-20 max-w-7xl mx-auto">
-        <div className="grid grid-cols-3 gap-8">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {BOOKS.map((book) => (
             <Book
               key={book.id}
